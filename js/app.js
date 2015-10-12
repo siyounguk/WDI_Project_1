5a091da82e08f9f558abb2e17e5a4209fa6ce50b
siyounguk <si.younguk@gmail.com>
2015-10-12 10:47:02 +0100

    added start functions

--- js/app.js
+++ js/app.js
@@ -1,28 +1,5 @@
 $(function() { 
 
-
-  var game = {}
-  var gameCard = $(".card")
-
-
-  var turnText = $("#turnText")
-  var choice1
-  var choice2
-  var playerOne = $("#player_one")
-  var playerTwo = $("#player_two")
-  var currentPlayer = 
-
-  
-  
-  game.turnChange = function(){
-    if (currentPlayer === playerOne){
-      currentPlayer = playerTwo 
-    }else{
-      currentPlayer = playerOne
-    }
-  }
-  var clicks = 0
-
   // var pic = []
   // pic[0] = "./images/left_image1.png" 
   // pic[1] = "./images/left_image6.png"
@@ -44,32 +21,58 @@ $(function() {
   // pic[10] = "./images/right_image6.png"
   // pic[14] = "./images/right_image7.png"
 
+  var game = {}
+  var gameCard = document.getElementsByClassName("card")
+  var turnText = $("#turnText")
+  var choice1
+  var choice2
+  var playerOne = document.getElementById("player_one")
+  var playerTwo = document.getElementById("player_two")
+  var currentPlayer 
+  var clicks = 0
+  addStartListener()
+
+  // game.turnChange = function(){
+  //   if (currentPlayer === playerOne){
+  //     currentPlayer = playerTwo 
+  //   }else{
+  //     currentPlayer = playerOne
+  //   }
+  // }
+ 
+  function addStartListener (){
+    var start = document.getElementById("start");
+    start.addEventListener("click",startGame)    
+  }
+
+  function startGame (){
+    currentPlayer = playerOne;
+    console.log (currentPlayer.innerHTML)
+    turnText.text( currentPlayer.innerHTML+ "'s" + " Turn")
+    addClickListener()
+  } 
+
+  function addClickListener(){
+    for(var i = gameCard.length -1; i >=0; i--){
+      gameCard[i].addEventListener("click", choice)
+    }
+  }
 
 
-  game.start = $(function(){
-    
-    $("#start").on("click", function(){
-        turnText.text("Player One's Turn")    
-        currentPlayer = playerOne
-        
-    })
-  })  
+  function choice () {
+  
 
-
-
-  game.choice = gameCard.on("click",function(){
-    
     var idToSRC = './images/'+ $(this).attr("id") +'.png';
 
     if (clicks === 0){ 
 
     
       $("img").attr("src", idToSRC);
-      console.log (this)
 
       choice1 = this.id
       clicks = 1
     }else if (clicks === 1){
+
       $("img").attr("src", idToSRC);
       console.log (this)
       choice2 = this.id
@@ -77,29 +80,23 @@ $(function() {
     }else if (clicks === 2){
       return false
     }
+  
 
-    game.checkCards = function(choice1, choice2){
+
+    function checkCards (choice1, choice2){
       if (choice1.charAt(choice1.length-1) === choice2.charAt(choice2.length-1)){
-        
+
+
         console.log ("correct")
 
       }else{
 
         console.log ("wrong")
       }
-
     }
 
     game.checkCards(choice1,choice2)
-
-
-
-  })
-
-
-
-  
-
+  }
 
 });
 
